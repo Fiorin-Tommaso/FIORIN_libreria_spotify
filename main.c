d6361229240bfb6de4152f9f0a954e3aa933f371
Fiorin-Tommaso <12999529@itisrossi.vi.it>
2025-10-28 17:56:51 +0100

    Fixies
    aggiunta funzione rimuovi_n che serve per levare lo /n dalla scritta prima del /0, aggiunto il count per le canzoni e sistemati gli errori dei puntatori

--- main.c
+++ main.c
@@ -2,71 +2,93 @@
 #include <stdlib.h>
 
 typedef struct canzone {
-    char *name;
-    char *artista;
+    char name[100];
+    char artista[100];
     int min;
     int sec;
-}canzone;
+} canzone;
 
 static int denision = 5;
+static int count = 0;
 
-void aggiunta();
+void aggiunta(canzone *lib);
+void visualizzaPlay(canzone *lib);
 
-void visualizzaPlay();
-
-int length();
+void rimuovi_n(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+            break;
+        }
+        i++;
+    }
+}
 
 int main(void) {
     canzone *libraries = (canzone *) malloc(denision * sizeof(canzone));
-
-    printf("premi 1 per: Aggiungi una canzone.\n");
-    printf("Premi 2 per: Visualizza libreria.\n");
-    printf("Premi qualsiasi tasto per uscire.\n");
-    int denio = 0;
-    scanf("%d", &denio);
-    switch (denio) {
-        case 1:
-            aggiunta(libraries);
-            break;
-        case 2:
-            visualizzaPlay(libraries);
-            break;
-        default:
+    int denisio = 0;
+    while (denisio == 0) {
+        int denio = 0;
+        printf("Premi 1 per: Aggiungi una canzone.\n");
+        printf("Premi 2 per: Visualizza libreria.\n");
+        printf("Premi qualsiasi tasto per uscire.\n");
+        if (scanf("%d", &denio) != 1) {
+            free(libraries);
             return 0;
-    }
+        }
+        getchar(); // leva dal buffer /n
 
-    free(libraries);
-    return 0;
-}
-
-void aggiunta(canzone **lib) {
-    int denis = length(**lib);
-    if (denis > denision) {
-        denision = denision * 2;
-        realloc(lib, denision * sizeof(canzone *));
-    }
-    printf("Inserisci in input il nome della canzone: \n");
-    fgets(lib[denis]->name, 100, stdin);
-    printf("Inserisci in input il nome dell'artista: \n");
-    fgets(lib[denis]->artista, 100, stdin);
-    printf("Inserisci in input i minuti della canzone: \n");
-    scanf("%d", &lib[denis]->min);
-    printf("Inserisci in input i second della canzone: \n");
-    scanf("%d", &lib[denis]->sec);
-}
-
-void visualizzaPlay(canzone **lib) {
-    int denis = length(**lib);
-    for (int i = 0; i < denis; i++) {
-        printf("Nome della canzone: %s\n",lib[i]->name);
-        printf("Nome della artista: %s\n",lib[i]->artista);
-        printf("Minuti della conzone: %d\n",lib[i]->min);
-        printf("Secondi della canzone: %d",lib[i]->sec);
+        switch (denio) {
+            case 1:
+                aggiunta(libraries);
+                break;
+            case 2:
+                visualizzaPlay(libraries);
+                break;
+            default:
+                free(libraries);
+                return 0;
+        }
     }
 }
 
-int length(canzone **str) {
-    int i = 0;
-    for (i; str[i] != NULL; i++);
-    return i;
+void aggiunta(canzone *lib) {
+    if (count >= denision) {
+        denision *= 2;
+        lib = realloc(lib, denision * sizeof(canzone));
+        if (lib == NULL) {
+            printf("Errore realloc\n");
+            exit(1);
+        }
+    }
+
+    printf("Inserisci il nome della canzone: ");
+    fgets(lib[count].name, 100, stdin);
+    rimuovi_n(lib[count].name);
+
+    printf("Inserisci il nome dell'artista: ");
+    fgets(lib[count].artista, 100, stdin);
+    rimuovi_n(lib[count].artista);
+
+    printf("Inserisci i minuti della canzone: ");
+    scanf("%d", &lib[count].min);
+    printf("Inserisci i secondi della canzone: ");
+    scanf("%d", &lib[count].sec);
+    getchar();
+
+    count++;
+}
+
+void visualizzaPlay(canzone *lib) {
+    if (count == 0) {
+        printf("La libreria è vuota.\n");
+        return;
+    }
+    for (int i = 0; i < count; i++) {
+        printf("\nCanzone %d:\n", i + 1);
+        printf("Nome: %s\n", lib[i].name);
+        printf("Artista: %s\n", lib[i].artista);
+        printf("Durata: %d:%02d\n", lib[i].min, lib[i].sec);
+    }
 }
